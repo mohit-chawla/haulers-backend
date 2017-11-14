206bc71c32d91d6047f4667a43058fb1d11a2f88
Mohit Chawla <mohitchawla@cse.ism.ac.in>
2017-11-14 00:17:26 -0500

    skeleton

---
+++ src/main/java/edu/cornell/haulers/HaulersProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.cornell.haulers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaulersProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaulersProjectApplication.class, args);
+	}
+}
