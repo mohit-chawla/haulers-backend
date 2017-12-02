0591c7254be10b32d68f1d8542e52b16d64c9e3a
Mohit Chawla <mohitchawla@cse.ism.ac.in>
2017-12-02 02:21:00 -0500

    added ant matchers for signup urls

--- src/main/java/edu/cornell/haulers/constants/HttpMappings.java
+++ src/main/java/edu/cornell/haulers/constants/HttpMappings.java
@@ -6,24 +6,24 @@ package edu.cornell.haulers.constants;
 public class HttpMappings {
 
 	public static final String DRIVER = "/driver";
-	public static final String DRIVER_ID = DRIVER+"/{id}";
+	public static final String DRIVER_ID = DRIVER + "/{id}";
 
-	public static final String DRIVER_JOBS = DRIVER+"/jobs";
-	public static final String DRIVER_JOBS_OPEN = DRIVER_JOBS+"/open";
-	public static final String DRIVER_JOBS_CLOSED = DRIVER_JOBS+"/closed";
+	public static final String DRIVER_JOBS = DRIVER + "/jobs";
+	public static final String DRIVER_JOBS_OPEN = DRIVER_JOBS + "/open";
+	public static final String DRIVER_JOBS_CLOSED = DRIVER_JOBS + "/closed";
 
-	public static final String DRIVER_UPDATE = DRIVER+"/update";
-	public static final String DRIVER_UPDATE_LOC = DRIVER_UPDATE+"/loc";
+	public static final String DRIVER_UPDATE = DRIVER + "/update";
+	public static final String DRIVER_UPDATE_LOC = DRIVER_UPDATE + "/loc";
 
 	public static final String CUSTOMER = "/customer";
-	public static final String CUSTOMER_ID = CUSTOMER+"/{id}";
+	public static final String CUSTOMER_ID = CUSTOMER + "/{id}";
 
-	public static final String CUSTOMER_JOBS = CUSTOMER+"/jobs";
-	public static final String CUSTOMER_JOBS_OPEN = CUSTOMER_JOBS+"/open";
-	public static final String CUSTOMER_JOBS_CLOSED = CUSTOMER_JOBS+"/closed";
+	public static final String CUSTOMER_JOBS = CUSTOMER + "/jobs";
+	public static final String CUSTOMER_JOBS_OPEN = CUSTOMER_JOBS + "/open";
+	public static final String CUSTOMER_JOBS_CLOSED = CUSTOMER_JOBS + "/closed";
 
-	public static final String CUSTOMER_UPDATE = CUSTOMER+"/update";
-	public static final String CUSTOMER_UPDATE_LOC = CUSTOMER_UPDATE+"/loc";
+	public static final String CUSTOMER_UPDATE = CUSTOMER + "/update";
+	public static final String CUSTOMER_UPDATE_LOC = CUSTOMER_UPDATE + "/loc";
 
 	public static final String JOB = "/job";
 	public static final String JOB_ID = JOB + "/{id}";
@@ -31,5 +31,7 @@ public class HttpMappings {
 	public static final String AUDIT = "/audit";
 	public static final String AUDIT_DB = AUDIT + "/db";
 	public static final String AUDIT_DB_STATS = AUDIT_DB + "/stats";
+	public static final String CUSTOMER_SIGNUP = CUSTOMER + "/signup";
+	public static final String DRIVER_SIGNUP = DRIVER + "/signup";
 
 }
